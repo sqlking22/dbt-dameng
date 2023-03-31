ca505931bff8a1fd5c11563b8e06a72efef1ece3
749888411 <749888411@qq.com>
2023-03-31 23:02:57 +0800

    update

--- dbt/include/dameng/macros/adapters.sql
+++ dbt/include/dameng/macros/adapters.sql
@@ -1,138 +1,83 @@
-{#
- Copyright (c) 2022, Oracle and/or its affiliates.
- Copyright (c) 2020, Vitor Avancini
+/* For examples of how to fill out the macros please refer to the postgres adapter and docs
+postgres adapter macros: https://github.com/dbt-labs/dbt-core/blob/main/plugins/postgres/dbt/include/postgres/macros/adapters.sql
+dbt docs: https://docs.getdbt.com/docs/contributing/building-a-new-adapter
+*/
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+{% macro dameng__alter_column_type(relation,column_name,new_column_type) -%}
+  {#
+    Changes column name or data type
+    1. Create a new column (w/ temp name and correct type)
+    2. Copy data over to it
+    3. Drop the existing column (cascade!)
+    4. Rename the new column to existing column
+  #}
+  {%- set tmp_column = column_name + "__dbt_alter" -%}
 
-     https://www.apache.org/licenses/LICENSE-2.0
+  {% call statement('alter_column_type 1', fetch_result=False) %}
+    alter table {{ relation }} add {{ tmp_column }} {{ new_column_type }}
+  {% endcall %}
+  {% call statement('alter_column_type 2', fetch_result=False) %}
+    update {{ relation  }} set {{ tmp_column }} = {{ column_name }}
+  {% endcall %}
+  {% call statement('alter_column_type 3', fetch_result=False) %}
+    alter table {{ relation }} drop column {{ column_name }} cascade constraints
+  {% endcall %}
+  {% call statement('alter_column_type 4', fetch_result=False) %}
+    alter table {{ relation }} rename column {{ tmp_column }} to {{ column_name }}
+  {% endcall %}
+{% endmacro %}
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-#}
-{% macro oracle__get_columns_in_query(select_sql) %}
-    {% call statement('get_columns_in_query', fetch_result=True, auto_begin=False) -%}
-        select * from (
-            {{ select_sql }}
-        ) dbt_sbq_tmp
-        where 1 = 0 and rownum < 1
-    {% endcall %}
-
-    {{ return(load_result('get_columns_in_query').table.columns | map(attribute='name') | list) }}
+{% macro dameng__check_schema_exists(information_schema,schema) -%}
+  {% if information_schema.database -%}
+    {{ adapter.verify_database(information_schema.database) }}
+  {%- endif -%}
+  {% call statement('check_schema_exists', fetch_result=True, auto_begin=False) %}
+    select count(*) from sys.all_users where username = upper('{{ schema }}')
+  {% endcall %}
+  {{ return(load_result('check_schema_exists').table) }}
 {% endmacro %}
 
 
-{% macro oracle__create_schema(relation, schema_name) -%}
-  {% if relation.database -%}
-    {{ adapter.verify_database(relation.database) }}
-  {%- endif -%}
-  {%- call statement('create_schema') -%}
-    -- Noop for not breaking tests, dameng
-    -- schemas are actualy users, we can't
-    -- create it here
-    select 'a' from dual
-  {%- endcall -%}
+{% macro dameng__create_schema(relation) -%}
+ {% if relation.database -%}
+   {{ adapter.verify_database(relation.database) }}
+ {%- endif -%}
+ {%- call statement('create_schema') -%}
+     create schema {{ relation.without_identifier().include(database=False) }}
+ {%- endcall -%}
 {% endmacro %}
 
-{% macro oracle__drop_schema(schema) -%}
-  {% if schema.database -%}
-    {{ adapter.verify_database(schema.database) }}
-  {%- endif -%}
+
+{% macro dameng__drop_schema(relation) -%}
   {%- call statement('drop_schema') -%}
-    -- from https://gist.github.com/rafaeleyng/33eaef673fc4ee98a6de4f70c8ce3657
-    BEGIN
-    FOR cur_rec IN (SELECT object_name, object_type
-                      FROM ALL_objects
-                      WHERE object_type IN
-                              ('TABLE',
-                                'VIEW',
-                                'PACKAGE',
-                                'PROCEDURE',
-                                'FUNCTION',
-                                'SEQUENCE',
-                                'TYPE',
-                                'SYNONYM',
-                                'MATERIALIZED VIEW'
-                              )
-                      AND upper(owner) = '{{ schema | upper }}')
-    LOOP
-        BEGIN
-          IF cur_rec.object_type = 'TABLE'
-          THEN
-              EXECUTE IMMEDIATE    'DROP '
-                                || cur_rec.object_type
-                                || ' "'
-                                || cur_rec.object_name
-                                || '" CASCADE CONSTRAINTS';
-          ELSE
-              EXECUTE IMMEDIATE    'DROP '
-                                || cur_rec.object_type
-                                || ' "'
-                                || cur_rec.object_name
-                                || '"';
-          END IF;
-        EXCEPTION
-          WHEN OTHERS
-          THEN
-              DBMS_OUTPUT.put_line (   'FAILED: DROP '
-                                    || cur_rec.object_type
-                                    || ' "'
-                                    || cur_rec.object_name
-                                    || '"'
-                                  );
-        END;
-    END LOOP;
-  END;
+    drop schema if exists {{ relation.without_identifier().include(database=False) }} cascade
   {%- endcall -%}
 {% endmacro %}
 
-{% macro oracle__create_table_as_backup(temporary, relation, sql) -%}
-  {%- set sql_header = config.get('sql_header', none) -%}
 
-  {{ sql_header if sql_header is not none }}
-  create {% if temporary -%}
-    global temporary
-  {%- endif %} table {{ relation.include(schema=(not temporary)) }}
-  {% if temporary -%} on commit preserve rows {%- endif %}
-  as
-    {{ sql }}
-{%- endmacro %}
-
-
-{% macro oracle__create_table_as(temporary, relation, sql) -%}
-  {%- set sql_header = config.get('sql_header', none) -%}
-  {%- set parallel = config.get('parallel', none) -%}
-  {%- set compression_clause = config.get('table_compression_clause', none) -%}
-
-  {{ sql_header if sql_header is not none }}
-
-  create {% if temporary -%}
-    global temporary
-  {%- endif %} table {{ relation.include(schema=(not temporary)) }}
-  {% if temporary -%} on commit preserve rows {%- endif %}
-  {% if not temporary -%}
-    {% if parallel %} parallel {{ parallel }}{% endif %}
-    {% if compression_clause %} {{ compression_clause }} {% endif %}
-  {%- endif %}
-  as
-    {{ sql }}
-
-{%- endmacro %}
-{% macro oracle__create_view_as(relation, sql) -%}
-  {%- set sql_header = config.get('sql_header', none) -%}
-
-  {{ sql_header if sql_header is not none }}
-  create or replace view {{ relation }} as
-    {{ sql }}
-
+{% macro dameng__drop_relation(relation) -%}
+  {% call statement('drop_relation', auto_begin=False) -%}
+   DECLARE
+     dne_942    EXCEPTION;
+     PRAGMA EXCEPTION_INIT(dne_942, -942);
+     attempted_ddl_on_in_use_GTT EXCEPTION;
+     pragma EXCEPTION_INIT(attempted_ddl_on_in_use_GTT, -14452);
+  BEGIN
+     SAVEPOINT start_transaction;
+     EXECUTE IMMEDIATE 'DROP {{ relation.type }} {{ relation }} cascade constraint';
+     COMMIT;
+  EXCEPTION
+     WHEN attempted_ddl_on_in_use_GTT THEN
+        NULL; -- if it its a global temporary table, leave it alone.
+     WHEN dne_942 THEN
+        NULL;
+  END;
+  {%- endcall %}
 {% endmacro %}
 
-{% macro oracle__get_columns_in_relation(relation) -%}
-  {% call statement('get_columns_in_relation', fetch_result=True) %}
+
+{% macro dameng__get_columns_in_relation(relation) -%}
+{% call statement('get_columns_in_relation', fetch_result=True) %}
       with columns as (
         select
             SYS_CONTEXT('userenv', 'DB_NAME') table_catalog,
@@ -188,136 +133,14 @@
         and upper(table_schema) = upper('{{ relation.schema }}')
         {% endif %}
       order by ordinal_position
-
   {% endcall %}
   {% set table = load_result('get_columns_in_relation').table %}
   {{ return(sql_convert_columns_in_relation(table)) }}
 {% endmacro %}
 
-{% macro oracle_escape_comment(comment) -%}
-  {% if comment is not string %}
-    {% do exceptions.raise_compiler_error('cannot escape a non-string: ' ~ comment) %}
-  {% endif %}
-  {%- set start_quote = "q'<" -%}
-  {%- set end_quote = ">'" -%}
-  {%- if end_quote in comment -%}
-    {%- do exceptions.raise_compiler_error('The string ' ~ end_quote ~ ' is not allowed in comments.') -%}
-  {%- endif -%}
-  {{ start_quote }}{{ comment }}{{ end_quote }}
-{%- endmacro %}
 
-{% macro oracle__alter_relation_comment(relation, comment) %}
-  {% set escaped_comment = oracle_escape_comment(comment) %}
-  {# "comment on table" even for views #}
-  comment on table {{ relation }} is {{ escaped_comment }}
-{% endmacro %}
-
-{% macro oracle__persist_docs(relation, model, for_relation, for_columns) -%}
-  {% if for_relation and config.persist_relation_docs() and model.description %}
-    {% do run_query(alter_relation_comment(relation, model.description)) %}
-  {% endif %}
-  {% if for_columns and config.persist_column_docs() and model.columns %}
-    {% set column_dict = model.columns %}
-    {% for column_name in column_dict %}
-      {% set comment = column_dict[column_name]['description'] %}
-      {% set escaped_comment = oracle_escape_comment(comment) %}
-      {% call statement('alter _column comment', fetch_result=False) -%}
-        comment on column {{ relation }}.{{ column_name }} is {{ escaped_comment }}
-      {%- endcall %}
-    {% endfor %}
-  {% endif %}
-{% endmacro %}
-
-{% macro oracle__alter_column_type(relation, column_name, new_column_type) -%}
-  {#
-    1. Create a new column (w/ temp name and correct type)
-    2. Copy data over to it
-    3. Drop the existing column (cascade!)
-    4. Rename the new column to existing column
-  #}
-  {%- set tmp_column = column_name + "__dbt_alter" -%}
-
-  {% call statement('alter_column_type 1', fetch_result=False) %}
-    alter table {{ relation }} add {{ tmp_column }} {{ new_column_type }}
-  {% endcall %}
-  {% call statement('alter_column_type 2', fetch_result=False) %}
-    update {{ relation  }} set {{ tmp_column }} = {{ column_name }}
-  {% endcall %}
-  {% call statement('alter_column_type 3', fetch_result=False) %}
-    alter table {{ relation }} drop column {{ column_name }} cascade constraints
-  {% endcall %}
-  {% call statement('alter_column_type 4', fetch_result=False) %}
-    alter table {{ relation }} rename column {{ tmp_column }} to {{ column_name }}
-  {% endcall %}
-
-{% endmacro %}
-
-{% macro oracle__drop_relation(relation) -%}
-  {% call statement('drop_relation', auto_begin=False) -%}
-   DECLARE
-     dne_942    EXCEPTION;
-     PRAGMA EXCEPTION_INIT(dne_942, -942);
-     attempted_ddl_on_in_use_GTT EXCEPTION;
-     pragma EXCEPTION_INIT(attempted_ddl_on_in_use_GTT, -14452);
-  BEGIN
-     SAVEPOINT start_transaction;
-     EXECUTE IMMEDIATE 'DROP {{ relation.type }} {{ relation }} cascade constraint';
-     COMMIT;
-  EXCEPTION
-     WHEN attempted_ddl_on_in_use_GTT THEN
-        NULL; -- if it its a global temporary table, leave it alone.
-     WHEN dne_942 THEN
-        NULL;
-  END;
-  {%- endcall %}
-{% endmacro %}
-
-{% macro oracle__truncate_relation(relation) -%}
-  {#-- To avoid `ORA-01702: a view is not appropriate here` we check that the relation to be truncated is a table #}
-  {% if relation.is_table %}
-    {% call statement('truncate_relation') -%}
-        truncate table {{ relation }}
-    {%- endcall %}
-  {% endif %}
-{% endmacro %}
-
-{% macro oracle__rename_relation(from_relation, to_relation) -%}
-  {% call statement('rename_relation') -%}
-    ALTER {{ from_relation.type }} {{ from_relation }} rename to {{ to_relation.include(schema=False) }}
-  {%- endcall %}
-{% endmacro %}
-
-{% macro oracle__information_schema_name(database) -%}
-  {% if database -%}
-    {{ adapter.verify_database(database) }}
-  {%- endif -%}
-  sys
-{%- endmacro %}
-
-{% macro oracle__list_schemas(database) %}
-  {% if database -%}
-    {{ adapter.verify_database(database) }}
-  {%- endif -%}
-  {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
-     	select username as "name"
-      from sys.all_users
-      order by username
-  {% endcall %}
-  {{ return(load_result('list_schemas').table) }}
-{% endmacro %}
-
-{% macro oracle__check_schema_exists(information_schema, schema) -%}
-  {% if information_schema.database -%}
-    {{ adapter.verify_database(information_schema.database) }}
-  {%- endif -%}
-  {% call statement('check_schema_exists', fetch_result=True, auto_begin=False) %}
-    select count(*) from sys.all_users where username = upper('{{ schema }}')
-  {% endcall %}
-  {{ return(load_result('check_schema_exists').table) }}
-{% endmacro %}
-
-{% macro oracle__list_relations_without_caching(schema_relation) %}
-  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+{% macro dameng__list_relations_without_caching(schema_relation) -%}
+{% call statement('list_relations_without_caching', fetch_result=True) -%}
     with tables as
       (select SYS_CONTEXT('userenv', 'DB_NAME') table_catalog,
          owner table_schema,
@@ -351,14 +174,33 @@
   {{ return(load_result('list_relations_without_caching').table) }}
 {% endmacro %}
 
-{% macro oracle__make_temp_relation(base_relation, suffix) %}
-    {% set dt = modules.datetime.datetime.now() %}
-    {% set dtstring = dt.strftime("%H%M%S%f") %}
-    {% set tmp_identifier = 'o$pt_' ~ base_relation.identifier ~ dtstring %}
-    {% set tmp_relation = base_relation.incorporate(
-                                path={"identifier": tmp_identifier, "schema": None}) -%}
+{% macro dameng__list_schemas(database) -%}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
+     	select username as "name"
+      from sys.all_users
+      order by username
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
+{% endmacro %}
 
-    {% do return(tmp_relation) %}
+{% macro dameng__rename_relation(from_relation, to_relation) -%}
+  {% call statement('rename_relation') -%}
+    ALTER {{ from_relation.type }} {{ from_relation }} rename to {{ to_relation.include(schema=False) }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro dameng__truncate_relation(relation) -%}
+  {% if relation.is_table %}
+    {% call statement('truncate_relation') -%}
+        truncate table {{ relation }}
+    {%- endcall %}
+  {% endif %}
+{% endmacro %}
+
+
+{% macro dameng__current_timestamp() -%}
+{# docs show not to be implemented currently. #}
+ now()
 {% endmacro %}
 
 {% macro get_database_name() %}
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,9 @@
-"""
-Copyright (c) 2022, Oracle and/or its affiliates.
-Copyright (c) 2020, Vitor Avancini
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-     https://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-"""
-
 import pytest
-import os
 
-# Import the functional fixtures as a plugin
+# import os
+# import json
+
+# Import the fuctional fixtures as a plugin
 # Note: fixtures with session scope need to be local
 
 pytest_plugins = ["dbt.tests.fixtures.project"]
@@ -29,16 +14,10 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 def dbt_profile_target():
     return {
         'type': 'dameng',
-        'threads': 4,
-        'user': os.getenv('DBT_DAMENG_USER'),
-        'pass': os.getenv('DBT_DAMENG_PASSWORD'),
-        'host': os.getenv('DBT_DAMENG_HOST'),
-        'schema': os.getenv('DBT_DAMENG_SCHEMA'),
-        'database': os.getenv('DBT_DAMENG_DATABASE'),
-        'port': os.getenv('DBT_DAMENG_PORT')
+        'threads': 1,
+        'user': 'SYSDBA',
+        'password': 'SYSDBA',
+        'host': 'localhost',
+        'database': 'TEST',
+        'port': 5236
     }
-
-
-@pytest.fixture(scope="class")
-def unique_schema(request, prefix) -> str:
-    return os.getenv('DBT_DAMENG_SCHEMA')
